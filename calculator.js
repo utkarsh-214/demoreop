c1314d6e8e421d4e76f997e5ed1ee589dc166edf
aviraj5121 <avirajnimja12@gmail.com>
2023-02-27 22:57:11 +0530

    js added and css changed

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
-import multiply from './multiply';
 
-print(multiply(5,6));
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", function() {
+        if (this.textContent === "=") {
+                output.value = eval(output.value);
+            }
+            else 
+            {
+                output.value += this.textContent;
+            }
+});
+});
+document.getElementById("C").addEventListener("click", function() {
+    document.getElementById("output").value="";    
+});
+document.getElementById("Del").addEventListener("click", function() {
+    document.getElementById("output").value= document.getElementById("output").value.slice(0,);    
+});
