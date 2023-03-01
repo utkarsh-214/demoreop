7153cd79ecdc1201e3e49b41752f90c9657b98f7
UTKARSH.s <utkarsh.sharma20@st.niituniversity.in>
2023-03-01 23:26:23 +0530

    reverting changes to main branch to correct mistakes

--- calculator.js
+++ calculator.js
@@ -1,17 +1,3 @@
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("click", function() {
-        if (this.textContent === "=") {
-                output.value = eval(output.value);
-            }
-            else 
-            {
-                output.value += this.textContent;
-            }
-});
-});
-document.getElementById("C").addEventListener("click", function() {
-    document.getElementById("output").value="";    
-});
-document.getElementById("Del").addEventListener("click", function() {
-    document.getElementById("output").value= document.getElementById("output").value.slice(0,);    
-});
+import multiply from './multiply';
+
+print(multiply(5,6));
